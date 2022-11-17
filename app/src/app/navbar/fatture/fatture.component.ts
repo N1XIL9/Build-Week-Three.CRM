41b9130b1e5cbf53652c9c92359718cde7087710
danydifilippo <danydifilippo@gmail.com>
2022-11-17 11:42:50 +0100

    init

--- app/src/app/navbar/fatture/fatture.component.ts
+++ app/src/app/navbar/fatture/fatture.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Fatture } from './fatture';
 import { FattureService } from './fatture.service';
@@ -10,14 +10,22 @@ import { FattureService } from './fatture.service';
 })
 export class FattureComponent implements OnInit {
 
-
+  @ViewChild('id',{read:ElementRef}) id?: ElementRef;
+  idNr?:number
   fatture: Fatture[] = []
 
   constructor(private fattureService: FattureService) { }
 
   ngOnInit(): void {
-    this.fattureService.getInvoice().subscribe(data => [this.fatture = data])
+    this.fattureService.getInvoice().subscribe((data) => {
+      this.fatture = data;
+      console.log(this.fatture)
+      this.idNr = this.id?.nativeElement.textContent
+      console.log(this.idNr);
+    })
+
   }
+
 
   onSubmit(form: NgForm){
     this.fattureService.addInvoice(form.value)
