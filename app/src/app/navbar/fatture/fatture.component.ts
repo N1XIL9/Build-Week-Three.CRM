ee7a3d7620fff65f755c8f7e2bc809ab4a00ed60
Lambo95 <lamberto.martini95@gmail.com>
2022-11-18 12:14:28 +0100

    up/non funziona

--- app/src/app/navbar/fatture/fatture.component.ts
+++ app/src/app/navbar/fatture/fatture.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Client } from '../clienti/client';
 import { ClientService } from '../clienti/client.service';
 import { Fatture } from './fatture';
@@ -8,56 +9,80 @@ import { FattureService } from './fatture.service';
 @Component({
   selector: 'app-fatture',
   templateUrl: './fatture.component.html',
-  styleUrls: ['./fatture.component.scss']
+  styleUrls: ['./fatture.component.scss'],
 })
 export class FattureComponent implements OnInit {
+  fattureArr: any[] = [];
+  dateIns = new Date();
+  statoFattura = 'NON PAGATA';
+  noShow = true;
+  error = undefined;
+  showAlert = false;
+  invoiceForm!: FormGroup;
+  idNr?: any;
+  newInvoiceID: any;
+  fattura: any;
+  cliente?: any;
 
-  fatture: Fatture[] = []
-  dateIns = new Date()
-  statoFattura = "NON PAGATA"
-  noShow = true
-  error = undefined
-  showAlert = false
-  invoiceForm: FormGroup = new FormGroup({})
-
-
-
-  constructor(private fattureService: FattureService, private clientService: ClientService) { }
+  constructor(
+    private fattureService: FattureService,
+    private clientService: ClientService,
+    private formBuilder: FormBuilder,
+    private router: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  this.fattureService.getInvoice().subscribe((data) =>{
-    this.fatture = data
-  })
+    this.fattureService.getInvoice().subscribe((respFatture) => {
+      this.fattureArr = respFatture;
+      console.log(respFatture);
+    });
 
-}
+    this.clientService.getClient().subscribe((respClienti) => {
+      this.cliente = respClienti;
+      console.log(respClienti);
+    });
 
-onSubmit(){
-  this.invoiceForm = new FormGroup({
-    "cliente": new FormGroup({
-      id: new FormControl(),
-      nome: new FormControl()
-    }),
-    "importo": new FormControl(),
-    "dataInserimento": new FormControl(new Date()),
-    "stato": new FormControl("NON PAGATA")
-  })
-
-    console.log(this.invoiceForm.value)
-
-  //   form.value.dataInserimento = this.dateIns
-  //   form.value.stato = this.statoFattura
-  //   this.fattureService.addInvoice(form.value).subscribe((data) =>{console.log(data);
-  // },
-  // err => {
-  //   console.log(err);
-  //   this.error = err.error;
-  //   this.showAlert = !this.showAlert
-  //   })
+    this.invoiceForm = this.formBuilder.group({
+      data: '',
+      numero: null,
+      anno: null,
+      importo: null,
+      stato: '',
+      cliente: new FormControl(null),
+      dataInserimento: new Date(),
+      // "dataUltimaModifica": "2021-03-24T21:32:06.375+00:00"
+    });
   }
 
+  onSubmit() {
+    this.router.params.subscribe((params) => {
+      const id = +params['id'];
+      this.clientService.getClientDetails(id).subscribe((data) => {
+        console.log(data);
+      });
 
+      // this.fattureService
+      //   .addInvoice(this.invoiceForm?.value)
+      //   .subscribe((data) => {
+      //     console.log(data);
+      //     this.newInvoiceID = data;
+      //     console.log(this.newInvoiceID);
+      //     this.fattureService
+      //       .getInvoiceDetails(this.newInvoiceID.id)
+      //       .subscribe((data) => {
+      //         console.log(data);
+      //         this.fattura = data;
+      //         this.idNr = this.fattura.cliente.id;
+      //         this.clientService
+      //           .getClientDetails(this.idNr)
+      //           .subscribe((data) => {
+      //             this.cliente = data;
+      //             console.log(this.cliente);
+      //           });
+      //       });
+      //   });
+    });
+  }
 
-  close(){}
-
-
+  close() {}
 }
